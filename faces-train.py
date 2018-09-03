12e28019bef884ab893705bd9dc1955d124a47c8
Saicharan Reddy <saicharan.reddy1@gmail.com>
2018-09-03 16:16:04 +0530

    Obtain training data

--- faces-train.py
+++ faces-train.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 from PIL import Image
+import cv2
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 image_dir = os.path.join(BASE_DIR, "images")
@@ -17,5 +18,11 @@ for root,dirs,files in os.walk(image_dir):
             label = os.path.basename(root).replace(" ","-").lower()
             print(label,path)
 
-            pil_image = Image.open(path).convert("L")#convert to grayscale
-            image_data = np.array("",)#convert into numpy array
+            pil_image = Image.open(path).convert("L") #convert to grayscale
+            image_array = np.array(pil_image,"uint8") #convert into numpy array
+            print(image_array)
+            faces = face_cascade.detectMultiScale(image_array,scaleFactor=1.5,minNeighbors=5) # detect x,y,w,h of faces in image
+
+            for (x,y,w,h) in faces: # obtain training data (append ROIs to x_train)
+                roi = image_array[y:y+h,x:x+w]
+                x_train.append(roi)
